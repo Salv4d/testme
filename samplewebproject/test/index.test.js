12d2dc0da3828181b3ecab0427560523b6835dfe
Salvador Oliveira <slsalvadorlucas@gmail.com>
2021-12-05 12:09:43 -0400

    Add validation for the email input submition

--- samplewebproject/test/index.test.js
+++ samplewebproject/test/index.test.js
@@ -7,3 +7,19 @@ it("has a text input", async () => {
 
   assert(input);
 });
+
+it("show sucess when submited 'valid' email", async () => {
+  const dom = await render("index.html");
+
+  const input = dom.window.document.querySelector("input");
+
+  input.value = "test@mail.com";
+
+  dom.window.document
+    .querySelector("form")
+    .dispatchEvent(new dom.window.Event("submit"));
+
+  const h1 = dom.window.document.querySelector("h1");
+
+  console.log(h1.innerHTML);
+});
